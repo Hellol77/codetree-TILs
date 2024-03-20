997ceffffbdfeaae788c72f998799b244e6e5e59
DongHyeonWon <dhe7700@naver.com>
2024-03-21 03:10:25 +0900

    Title: 그래프 탐색, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240321/그래프 탐색/graph-traversal.py
+++ 240321/그래프 탐색/graph-traversal.py
@@ -7,6 +7,7 @@ l=[[0 for i in range(n+1)]for i in range(n+1)]
 for i in range(m):
     x,y=map(int,sys.stdin.readline().split())
     l[x][y]=1
+    l[y][x]=1
 visited=[False for i in range(n+1)]
 answer=0
 def dfs(start):
