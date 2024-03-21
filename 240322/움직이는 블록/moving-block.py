1aedd422b414a4008c10a96cb25d660e4ad93a65
DongHyeonWon <dhe7700@naver.com>
2024-03-22 00:03:38 +0900

    Title: 움직이는 블록, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240322/움직이는 블록/moving-block.py
@@ -0,0 +1,19 @@
+import sys
+
+l=[]
+n=int(sys.stdin.readline())
+for i in range(n):
+    block=int(sys.stdin.readline())
+    l.append(block)
+
+s=sum(l)
+s=s//4
+answer=0
+for i in l:
+    if i==s:
+        continue
+    elif i <s:
+        answer+=abs(i-s)
+    elif s<i:
+        answer+=abs(i-s)
+print(answer//2)
